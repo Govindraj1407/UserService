0c2883d5409ac397587c0ab558fd77b925568532
Govindraj Kanniyappan <govindraj.kanniyappan@contractor.tranetechnologies.com>
2022-09-02 21:35:37 +0530

    Userservice

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.ViewModels;
+using UserService.Services;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserInfoController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UserInfoController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] UserInfo userInfo)
+        {
+            if(this.ModelState.IsValid && this.userService.CreateUser(userInfo))
+            {
+                return this.NoContent();
+            }
+            else
+            {
+                return this.BadRequest("Invalid request, please check the request parameter.");
+            }
+           
+        }
+    }
+}
